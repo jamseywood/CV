3f53c4de98a2939efba2dcb531a098463641d7db
James Worwood <james@worwood.co.za>
2023-11-15 17:03:34 +0000

    small correction

--- src/app/home/profile/profile.component.ts
+++ src/app/home/profile/profile.component.ts
@@ -15,9 +15,9 @@ export class ProfileComponent implements OnInit {
   public profile: any =
     {
       title: 'Cover Letter:', data:
-        '<p>I am a self-driven software developer with 8 years of experience in helping to deliver projects on time and on budget. I have a special interest in understanding the needs of business and translating that into elegant solutions,'
+        '<p>I am a self-driven software developer with 10 years of experience in helping to deliver projects on time and on budget. I have a special interest in understanding the needs of business and translating that into elegant solutions,'
         + 'and providing a valuable product for business and translating that into elegant solutions, and providing a valuable product for business and translating that into elegant solutions, and providing a valuable product for '
-        + 'the end-users. My focus for the last 3 years has been in front-end development specifically <b>Angular 7+, CSS, HTML, Angular material, Bootstrap</b>, and a few other technologies. I have a special interest for the dev-ops realm '
+        + 'the end-users. My focus for the last 6 years has been in front-end development specifically <b>Angular 7+, CSS, HTML, Angular material, Bootstrap</b>, and a few other technologies. I have a special interest for the dev-ops realm '
         + 'as well, with some experience in <b>Docker, Jenkins, Azure, Kubernetes</b></p>',
       contact: 'CONTACT', sp: 'SOCIAL PROFILES'
     };
